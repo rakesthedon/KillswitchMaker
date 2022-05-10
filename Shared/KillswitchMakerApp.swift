31e9c942cd70231327d168b699f04783ae306be6
Yannick Jacques <yannick.jacques@shopify.com>
2022-05-10 12:15:44 -0400

    Initial Commit

---
+++ Shared/KillswitchMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KillswitchMakerApp.swift
+//  Shared
+//
+//  Created by Yannick Jacques on 2022-05-10.
+//
+
+import SwiftUI
+
+@main
+struct KillswitchMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
